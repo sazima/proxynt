283c2bc0d5fb637aa8f027d3926231980e26c302
sazima <w@wktadmin.com>
2022-11-08 12:40:49 +0800

    fix: key error

--- common/pool.py
+++ common/pool.py
@@ -2,6 +2,7 @@ import select
 import socket
 from typing import Dict, List
 
+from common.logger_factory import LoggerFactory
 from constant.system_constant import SystemConstant
 
 """
@@ -56,8 +57,12 @@ class EPool(SocketLoop):
             events = self.poll.poll(SystemConstant.DEFAULT_TIMEOUT)
             # 事件是一个`(fileno, 事件code)`的元组
             for fileno, event in events:
-                client = self.fileno_to_client[fileno]
+                client = self.fileno_to_client.get(fileno)
+                if client is not None:
+                    LoggerFactory.get_logger().warn(f'key error, {fileno}, self.fileno_to_client: {self.fileno_to_client}')
+                    continue
                 self.call_back_function[0](client)
+
 
     def register(self, s: socket.socket):
         self.fileno_to_client[s.fileno()] = s
