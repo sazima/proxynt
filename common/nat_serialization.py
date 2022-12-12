3c07b1b8bd8d1dc183ed6f906f0019808aebede8
sazima <w@wktadmin.com>
2022-12-12 08:56:03 +0800

    报文说明

--- common/nat_serialization.py
+++ common/nat_serialization.py
@@ -8,10 +8,18 @@ from entity.message.tcp_over_websocket_message import TcpOverWebsocketMessage
 
 
 class NatSerialization:
+    """
+        定义协议:
+        第0字节: 为报文类型 类型参考: MessageTypeConstant
+        第1字节之后是数据:
+            如果类型是转发tcp报文: 前1-9字节为两个uint32数字(低位在前), 分别代表name长度和tcp报文的长度, 第9字节之后是uid, name和tcp报文
+            如果类型是配置信息: 1-最后二进制转字符串是一个 Json 字符串
+            如果类型websocket心跳: 长度为1个字节
+
+        暂时没有加密报文, 因此使用的时候需要自己配置 Https
+    """
 
     # 报文形式: 类型, 数据
-    # 如果是config的: name长度, name, remote_port长度, remote_port, local_port长度, local_port, local_ip长度, local_ip
-    # 如果是socket数据: 后面是  uid:[32位], name长度, name, 报文
     @classmethod
     def dumps(cls, data: MessageEntity, key: str) -> bytes:
         b = b''
@@ -22,7 +30,7 @@ class NatSerialization:
             uid = data_content['uid']  # 长度32
             name = data_content['name']
             bytes_ = data_content['data']
-            # I是uint32, 占4位, unsigned __int32	0 到 4,294,967,295;  uid是固定32
+            # I是uint32, 占4个字节, unsigned __int32	0 到 4,294,967,295;  uid是固定32
             b = type_.encode() + struct.pack(f'II32s{len(name.encode())}s{len(bytes_)}s', len(name.encode()), len(bytes_), uid.encode(), name.encode(),  bytes_)
         elif type_ == MessageTypeConstant.PUSH_CONFIG:
             b = type_.encode() + json.dumps(data).encode()
@@ -35,7 +43,7 @@ class NatSerialization:
         byte_data = EncryptUtils.decode(byte_data, key)
         type_ = byte_data[0:1]
         if type_ == MessageTypeConstant.WEBSOCKET_OVER_TCP.encode():
-            # I是uint32, 占4位, unsigned __int32	0 到 4,294,967,295;  uid是固定32
+            # I是uint32, 占4个字节, unsigned __int32	0 到 4,294,967,295;  uid是固定32
             len_name, len_bytes = struct.unpack('II', byte_data[1:9])
             uid, name, socket_dta = struct.unpack(f'32s{len_name}s{len_bytes}s', byte_data[9:])
             data: TcpOverWebsocketMessage = {
@@ -62,11 +70,17 @@ class NatSerialization:
 
 if __name__ == '__main__':
 
+    def _print_commend(msg):
+        print(''.join("b'{}'".format(''.join('\\x{:02x}'.format(b) for b in msg))))
+
+
     data = {'type_': '2',
             'data': {'name': 'ssh',
                      'data': b'SSH-2.0-OpenSSH_7.8\r\n',
                      'uid': 'e18fe62fa05f446db95236c9826bfdd6'}}
     a = NatSerialization.dumps(data, '')
+    _print_commend(a)
+
     # print(pickle.dumps(a))
     print(a)
     b = NatSerialization.loads(a, '')
